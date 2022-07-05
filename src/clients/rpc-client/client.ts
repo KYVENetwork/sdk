679c2b1bdf3e06ce5774c7dda515bbd2971090c6
RuslanGlaznyov <ruslan.glaznyov@gmail.com>
2022-07-05 22:47:47 +0300

    feature: draft sign trx

--- src/clients/rpc-client/client.ts
+++ src/clients/rpc-client/client.ts
@@ -25,6 +25,63 @@ export default class KyveClient {
   }
 
   async signString(data: string) {
+    // const id = await this.nativeClient.getChainId()
+    // console.log(id);
+    /*const { accountNumber, sequence } = (await this.nativeClient.getSequence(this.account.address))!;
+    const feeAmount = coins(2000, "tkyve");
+    const gasLimit = 200000;
+    this.signer.signDirect
+    const authInfoBytes = makeAuthInfoBytes([{ pubkey, sequence }], feeAmount, gasLimit, sequence);
+    */
+    // const signDoc = makeSignDoc(new Uint8Array(Buffer.from(data, "utf-8")), new Uint8Array(), id, 0);
+
+    // {
+    //   "type": "cosmos-sdk/StdTx",
+    //     "value": {
+    //   "msg": [
+    //     {
+    //       "type": "sign/MsgSignData",
+    //       "value": {
+    //         "signer": "cosmos1hftz5ugqmpg9243xeegsqqav62f8hnywsjr4xr",
+    //         "data": "cmFuZG9t"
+    //       }
+    //     }
+    //   ],
+    //       "fee": {
+    //     "amount": [],
+    //         "gas": "0"
+    //   },
+    //   "signatures": [
+    //     {
+    //       "pub_key": {
+    //         "type": "tendermint/PubKeySecp256k1",
+    //         "value": "AqnDSiRoFmTPfq97xxEb2VkQ/Hm28cPsqsZm9jEVsYK9"
+    //       },
+    //       "signature": "8y8i34qJakkjse9pOD2De+dnlc4KvFgh0wQpes4eydN66D9kv7cmCEouRrkka9tlW9cAkIL52ErB+6ye7X5aEg=="
+    //     }
+    //   ],
+    //       "memo": ""
+    // }
+    // // }
+    // draft to create sign transaction
+    // const txBodyFields = {
+    //   typeUrl: "/cosmos.tx.v1beta1.TxBody",
+    //   value: {
+    //     messages: [
+    //       {
+    //         typeUrl: "sign/MsgSignData",
+    //         value: {
+    //           "signer": "cosmos1hftz5ugqmpg9243xeegsqqav62f8hnywsjr4xr",
+    //           "data": "cmFuZG9t"
+    //         },
+    //       },
+    //     ],
+    //   },
+    // }
+    // const registry = new Registry([...defaultRegistryTypes, ...KyveRegistryTx.registry]);
+    // const txBodyBytes = registry.encode(txBodyFields);
+    // const signDoc1 = makeSignDoc(txBodyBytes, authInfoBytes1, id, 0);
+    // console.log(txBodyBytes);
     const signDoc = makeSignDoc(new Uint8Array(Buffer.from(data, "utf-8")), new Uint8Array(), await this.nativeClient.getChainId(), 0);
     return this.signer.signDirect(this.account.address, signDoc)
   }
