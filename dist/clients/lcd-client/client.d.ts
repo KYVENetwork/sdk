af5b4f84377d909781221b29c5ffcb408d355f14
RuslanGlaznyov <ruslan.glaznyov@gmail.com>
2022-07-26 14:59:39 +0300

    yarn build .

--- dist/clients/lcd-client/client.d.ts
+++ dist/clients/lcd-client/client.d.ts
@@ -1,6 +1,4 @@
 import { KyveRegistryLCDClient } from "./registry/v1beta1/query";
-import { AuthExtension, BankExtension, DistributionExtension, GovExtension, LcdClient, MintExtension, SlashingExtension, StakingExtension, SupplyExtension } from "@cosmjs/launchpad";
-declare type LCDClientType = LcdClient & AuthExtension & BankExtension & DistributionExtension & GovExtension & MintExtension & SlashingExtension & StakingExtension & SupplyExtension;
 declare class KyveLCDClient {
     registry: {
         v1beta1: KyveRegistryLCDClient;
@@ -8,9 +6,6 @@ declare class KyveLCDClient {
     constructor(restEndpoint: string);
 }
 export declare type KyveLCDClientType = {
-    cosmos: {
-        v1beta1: LCDClientType;
-    };
     kyve: KyveLCDClient;
 };
 export declare function createKyveLCDClient(restEndpoint: string): KyveLCDClientType;
